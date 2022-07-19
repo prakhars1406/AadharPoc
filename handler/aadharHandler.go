f5f7ca85c336dedba065cfa425526662f4d05d8d
abhishektakale <abhishektakale@chainworks.io>
2022-07-19 12:48:07 +0530

    added auto-download code

--- handler/aadharHandler.go
+++ handler/aadharHandler.go
@@ -2,7 +2,11 @@ package handler
 
 import (
 	"Aadhar_POC/database"
+	"fmt"
+	"io"
+	"net"
 	"net/http"
+	"time"
 )
 
 func AddAadharHandler(dataStoreClient database.MongoClient) http.HandlerFunc {
@@ -14,7 +18,26 @@ func AddAadharHandler(dataStoreClient database.MongoClient) http.HandlerFunc {
 
 func GetAadharHandler(dataStoreClient database.MongoClient) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(http.StatusOK)
+		timeout := time.Duration(5) * time.Second
+		transport := &http.Transport{
+			ResponseHeaderTimeout: timeout,
+			Dial: func(network, addr string) (net.Conn, error) {
+				return net.DialTimeout(network, addr, timeout)
+			},
+			DisableKeepAlives: true,
+		}
+
+		client := &http.Client{
+			Transport: transport,
+		}
+		resp, err := client.Get("https://file.io/aNuhLF8xQHV1")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+
+		writer.Header().Set("Content-Type", "application/octet-stream")
+		writer.Header().Set("Content-Disposition", "attachment; filename=text.xml")
+		io.Copy(writer, resp.Body)
 	}
 }
